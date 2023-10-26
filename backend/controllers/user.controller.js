e51fb5505f6759f841b26de4e6dfa683b8085c2a
AishaYanez <aishayanezdelaguardia@alumno.ieselrincon.es>
2023-10-26 09:25:16 +0100

    Add project on Main branch

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -65,8 +65,10 @@ exports.update = (req, res) => {
     email: req.body.email,
     password: req.body.password,
     user_code: req.body.user_code,
-    filename: req.file ? req.file.filename : ""
+    filename: req.file || ""
   };
+
+  console.log(user.email + "     " + user.filename);
 
   User.update(user, {
     where: { id: id }
@@ -93,7 +95,23 @@ exports.update = (req, res) => {
 exports.delete = (req, res) => {
   const id = req.params.id;
 
-  User.delete({
+  User.destroy({
     where: { id: id }
   })
+    .then(num => {
+      if (num === 1) {
+        res.status(200).json({
+          message: "User was deleted successfully."
+        });
+      } else {
+        res.status(404).json({
+          message: `Cannot delete User with id=${id}. Maybe User was not found.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error deleting User with id=" + id
+      });
+    });
 };
